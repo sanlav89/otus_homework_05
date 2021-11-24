f39cc4b25922bb31444999bc0364e74b952fe91e
sanlav89 <sanlav89@mail.ru>
2021-11-24 17:27:19 +0300

    for question

--- main.cpp
+++ main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
-#include <cassert>
-#include "matrix.h"
 
 #define UNUSED(value) (void)value
-
-using namespace matrix;
 
 template<typename T>
 class foo
@@ -30,21 +26,14 @@ private:
 
 int main(void)
 {
-//    std::cout << "Hello from Matrix" << std::endl;
-
-//    Matrix<int, -1> matrix;
-//    assert(matrix.size() == 0);
-//    int a = matrix[0][0];
-//    assert(a == -1);
-//    matrix[100][100] = 314;
-//    assert(matrix[100][100] == 314);
-//    std::cout << matrix.size() << std::endl;
-//    assert(matrix.size() == 1);
-
-
     foo<int> f;
     f[10] = 0;
     auto a = f[10];
+
+//  output:
+//    T& foo<T>::operator[](int) [with T = int]
+//    T& foo<T>::operator[](int) [with T = int]
+
     UNUSED(a);
 
     return 0;
