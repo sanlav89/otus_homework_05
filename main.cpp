1a5c8e6710bd19389d949c407fce1c77ae75e503
sanlav89 <sanlav89@mail.ru>
2021-11-25 16:00:20 +0300

    after quastion

--- main.cpp
+++ main.cpp
@@ -1,40 +1,21 @@
 #include <iostream>
+#include <cassert>
+#include "matrix.h"
 
-#define UNUSED(value) (void)value
-
-template<typename T>
-class foo
-{
-public:
-    foo() = default;
-
-    const T &operator[](int) const
-    {
-        std::cout << __PRETTY_FUNCTION__ << std::endl;
-        return tmp;
-    }
-
-    T &operator[](int)
-    {
-        std::cout << __PRETTY_FUNCTION__ << std::endl;
-        return tmp;
-    }
-
-private:
-    T tmp;
-};
+using namespace matrix;
 
 int main(void)
 {
-    foo<int> f;
-    f[10] = 0;
-    auto a = f[10];
+    std::cout << "Hello from Matrix" << std::endl;
 
-//  output:
-//    T& foo<T>::operator[](int) [with T = int]
-//    T& foo<T>::operator[](int) [with T = int]
-
-    UNUSED(a);
+    Matrix<int, -1> matrix;
+    assert(matrix.size() == 0);
+    int a = matrix[0][0];
+    assert(a == -1);
+    matrix[100][100] = 314;
+    assert(matrix[100][100] == 314);
+    std::cout << matrix.size() << std::endl;
+    assert(matrix.size() == 1);
 
     return 0;
 }
