13c018741490bc4fb83b51ddaedf975ea87380b7
sanlav89 <sanlav89@mail.ru>
2021-11-28 22:02:50 +0300

    general n-order matrix

--- main.cpp
+++ main.cpp
@@ -4,18 +4,21 @@
 
 using namespace matrix;
 
+#define UNUSED(value) (void)value
+
 int main(void)
 {
     std::cout << "Hello from Matrix" << std::endl;
 
-    Matrix<int, -1> matrix;
+    Matrix<int, -1, 3> matrix;
     assert(matrix.size() == 0);
-    int a = matrix[0][0];
-    assert(a == -1);
-    matrix[100][100] = 314;
-    assert(matrix[100][100] == 314);
-    std::cout << matrix.size() << std::endl;
-    assert(matrix.size() == 1);
+//    std::cout << matrix[0][0][0] << std::endl;
+//    auto a = matrix[0][0];
+//    assert(a == -1);
+    // assert(matrix.size() == 0); // Уже здесь ошибка
+    matrix[100][100][0] = 314;
+    assert(matrix[100][100][0] == 314);
+    assert(matrix.size() == 1);   // size = 2
 
     return 0;
 }
--- matrix.h
+++ matrix.h
@@ -4,61 +4,156 @@
 
 namespace matrix {
 
-    template<typename T, T Default, int Order = 1>
-    class Matrix
+//    template<typename T, T Default, int Order = 1>
+//    class Matrix
+//    {
+
+//    private:
+
+//        class Row
+//        {
+//            friend class Matrix;
+
+//        public:
+
+//            const T &operator[](int col) const
+//            {
+//                auto it = m_parent.m_matrix.find({m_row, col});
+//                auto end = m_parent.m_matrix.end();
+//                if (it == end) {
+//                    return Default;
+//                } else {
+//                    return m_parent.m_matrix[{m_row, col}];
+//                }
+//            }
+
+//            T &operator[](int col)
+//            {
+//                auto it = m_parent.m_matrix.find({m_row, col});
+//                auto end = m_parent.m_matrix.end();
+//                if (it == end) {
+//                    m_parent.m_matrix.insert({{m_row, col}, -1});
+//                }
+//                return m_parent.m_matrix[{m_row, col}];
+//            }
+
+//        private:
+//            Row(Matrix &parent, int row) : m_parent(parent), m_row(row) {}
+//            Matrix &m_parent;
+//            int m_row;
+
+//        };
+
+//        std::map<std::pair<int, int>, T> m_matrix;
+
+//    public:
+
+//        Matrix() = default;
+
+//        Row operator[](int row)
+//        {
+//            return Row(*this, row);
+//        }
+
+//        std::size_t size() const
+//        {
+//            return m_matrix.size();
+//        }
+//    };
+
+
+//template<typename T>
+
+
+/**
+ * Matrix is a class template that implements a multi-dimensional storage
+ * of specific type T. The Container is responsible to how these Ts will
+ * be stored and retrieved from memory.
+ * This is the general case for dimensions > 1. For the simple case of 1
+ * dimension see below.
+ */
+template <typename T, T Default, int Dim = 2>
+class Matrix
+{
+
+    typedef Matrix<T, Default, Dim-1> itemType;
+    std::map<int, itemType> m_data;
+
+public:
+    /**
+     * This returns an item of the array m_data which is probably a matrix
+     * of less dimensions that can be further accessed be the same operator
+     * for resolving another dimension.
+     */
+    itemType& operator[](int idx)
     {
+        return m_data[idx];
+    }
 
-    private:
+    const itemType& operator[](int idx) const
+    {
+        return m_data[idx];
+    }
 
-        class Row
-        {
-            friend class Matrix;
+    size_t size() const
+    {
+        return m_data.size();
+    }
+};
 
-        public:
+/**
+ * This is the simple case of a one-dimensional matrix which is technically
+ * an array in its simplest case.
+ */
+template <typename T, T Default>
+class Matrix<T, Default, 1>
+{
+    /**
+     * Here we are defining an array of itemType which is defined to be T.
+     * so we are actually defining an array of T.
+     */
+//    typedef T itemType;
 
-            const T &operator[](int col) const
-            {
-                auto it = m_parent.m_matrix.find({m_row, col});
-                auto end = m_parent.m_matrix.end();
-                if (it == end) {
-                    return Default;
-                } else {
-                    return m_parent.m_matrix[{m_row, col}];
-                }
-            }
+public:
 
-            T &operator[](int col)
-            {
-                auto it = m_parent.m_matrix.find({m_row, col});
-                auto end = m_parent.m_matrix.end();
-                if (it == end) {
-                    m_parent.m_matrix.insert({{m_row, col}, -1});
-                }
-                return m_parent.m_matrix[{m_row, col}];
-            }
+//    class MatrixType
+//    {
+//        friend class Matrix;
 
-        private:
-            Row(Matrix &parent, int row) : m_parent(parent), m_row(row) {}
-            Matrix &m_parent;
-            int m_row;
+//    public:
+//        MatrixType(const T &value) : m_value(value) {}
 
-        };
+//    private:
 
-        std::map<std::pair<int, int>, T> m_matrix;
+//        MatrixType(Matrix &parent, const T &value)
+//            : m_value(value)
+//            , m_matrix(parent)
+//        {}
 
-    public:
+//        T m_value;
+//        Matrix &m_matrix;
+//    };
 
-        Matrix() = default;
+private:
 
-        Row operator[](int row)
-        {
-            return Row(*this, row);
-        }
+    std::map<int, T> m_data;
 
-        std::size_t size() const
-        {
-            return m_matrix.size();
-        }
-    };
+public:
+    T& operator[](int idx)
+    {
+        return m_data[idx];
+    }
+
+    const T& operator[](int idx) const
+    {
+        return m_data[idx];
+    }
+
+    size_t size() const
+    {
+        return m_data.size();
+    }
+};
+
 
 }
