344acf7c516d76e1c6d60d2816fa246d311bcde3
sanlav89 <sanlav89@mail.ru>
2021-11-29 11:47:05 +0300

    minimum requirements are met

--- main.cpp
+++ main.cpp
@@ -1,23 +1,47 @@
-#include <iostream>
-#include <cassert>
 #include "matrix.h"
 
 using namespace matrix;
 
-#define UNUSED(value) (void)value
-
 int main(void)
 {
-    Matrix<int, -1> matrix;
-    assert(matrix.size() == 0);
-    auto a = matrix[0][0];
-    assert(a == -1);
-    assert(matrix.size() == 0);
-    matrix[100][100] = 314;
-    assert(matrix[100][100] == 314);
-    assert(matrix.size() == 1);
+    // Example
+//    example();
 
-    std::cout << "Done" << std::endl;
+    // Task
+    const auto matrix_size = 10;
+    const auto min_index = 1;
+    const auto max_index = 8;
+
+    Matrix<int, 0> matrix;
+    for (auto i = 0; i < matrix_size; i++) {
+        matrix[i][i] = i;
+        matrix[i][matrix_size - (i + 1)] = matrix_size - (i + 1);
+    }
+
+    std::cout << "Matrix from [" << min_index << "," << min_index << "] to ["
+              << max_index << "," << max_index << "]" << std::endl;
+    for (auto i = min_index; i <= max_index; i++) {
+        for (auto j = min_index; j <= max_index; j++) {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    std::cout << "Size:" << matrix.size() << std::endl;
+    std::cout << "Filled elements:" << std::endl;
+
+    for (auto c : matrix) {
+        int x;
+        int y;
+        int v;
+        std::tie(x, y, v) = c;
+        std::cout << "(" << x << "," << y << ") = " << v << std::endl;
+    }
+
+    std::cout << "Canonical :" << std::endl;
+    std::cout << "((matrix[100][100] = 314) = 0) = 217;" << std::endl;
+    ((matrix[100][100] = 314) = 0) = 217;
+    std::cout << "(" << 100 << "," << 100 << ") = " << matrix[100][100] << std::endl;
 
     return 0;
 }
--- matrix.h
+++ matrix.h
@@ -1,6 +1,9 @@
 #pragma once
+#include <iostream>
+#include <cassert>
 #include <map>
 #include <vector>
+#include <tuple>
 
 namespace matrix {
 
@@ -18,30 +21,47 @@ private:
 
         T &operator=(const T &value)
         {
-            m_parent.m_matrix[{m_row, m_col}] = value;
-            return m_parent.m_matrix[{m_row, m_col}];
+            if (value != m_default) {
+                m_parent.m_matrix[{m_row, m_col}] = value;
+                return m_parent.m_matrix[{m_row, m_col}];
+            } else {
+                return m_default;
+            }
         }
 
         bool operator==(const T &value)
         {
-            T tmp = Default;
-            auto it = m_parent.m_matrix.find({m_row, m_col});
-            auto end = m_parent.m_matrix.end();
-            if (it != end) {
-                tmp = m_parent.m_matrix[{m_row, m_col}];
-            }
-            return tmp == value;
+            return find() == value;
+        }
+
+        friend std::ostream& operator<<(std::ostream& os, const MatrixType& mt)
+        {
+            os << mt.find();
+            return os;
         }
 
     private:
         Matrix &m_parent;
         int m_row;
         int m_col;
+        T m_default;
+
+        T find() const
+        {
+            T tmp = m_default;
+            auto it = m_parent.m_matrix.find({m_row, m_col});
+            auto end = m_parent.m_matrix.end();
+            if (it != end) {
+                tmp = m_parent.m_matrix[{m_row, m_col}];
+            }
+            return tmp;
+        }
 
         MatrixType(Matrix &parent, int row, int col)
             : m_parent(parent)
             , m_row(row)
             , m_col(col)
+            , m_default(Default)
         {}
     };
 
@@ -67,6 +87,42 @@ private:
 
 public:
 
+    using map_iter = typename std::map<std::pair<int, int>, T>::iterator;
+
+    // Iterator Class
+    class iterator {
+    public:
+        iterator() : m_it(map_iter(nullptr)) {}
+
+        iterator(map_iter it) : m_it(it) {}
+
+        bool operator==(const iterator& other) const
+        {
+            return m_it == other.m_it;
+        }
+
+        bool operator!=(const iterator& other) const
+        {
+            return !(*this == other);
+        }
+
+        std::tuple<int, int, T> operator*() const
+        {
+            return std::make_tuple((*m_it).first.first,
+                                   (*m_it).first.second,
+                                   (*m_it).second);
+        }
+
+        iterator& operator++()
+        {
+            ++m_it;
+            return *this;
+        }
+
+    private:
+        map_iter m_it;
+    };
+
     Matrix() = default;
 
     Row operator[](int row)
@@ -78,6 +134,47 @@ public:
     {
         return m_matrix.size();
     }
+
+    iterator begin()
+    {
+        return iterator(m_matrix.begin());
+    }
+
+    iterator end()
+    {
+        return iterator(m_matrix.end());
+    }
 };
+
+// Task Example
+void example()
+{
+    // Example
+
+    std::cout << "Example: " << std::endl;
+
+    Matrix<int, -1> matrix;
+    assert(matrix.size() == 0);
+    auto a = matrix[0][0];
+    assert(a == -1);
+    assert(matrix.size() == 0);
+    matrix[100][100] = 314;
+    assert(matrix[100][100] == 314);
+    assert(matrix.size() == 1);
+    // выведется одна строка
+    // 100100314
+    for (auto c : matrix) {
+        int x;
+        int y;
+        int v;
+        std::tie(x, y, v) = c;
+        std::cout << x << y << v << std::endl;
+    }
+
+    ((matrix[100][100] = 314) = 0) = 217;
+    assert(matrix[100][100] == 217);
+
+    std::cout << std::endl;
+}
 
 }
